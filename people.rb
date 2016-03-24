2fc33647c46cebd7f9ac5a41bda258f37bd1ef9b
houtaiw61@gmail.com <houtaiw61@gmail.com>
2016-03-24 14:16:02 -0400

    Called teach method on chris and learn method on cristina. Also added explanation why you can't call teach on cristina and learn on chris.

--- people.rb
+++ people.rb
@@ -31,3 +31,10 @@ chris.greeting
 # Creating instance of Student Cristina and call the greeting method from the parent class
 cristina = Student.new("Cristina")
 cristina.greeting
+
+# calling teach method on instructor and learn method on Student
+chris.teach
+cristina.learn
+
+# The reason why calling teach method on a student class doesn't work and vice versa learn method on Instructor is because both those methods don't fall under each others class.
+# They are both class methods. Now if both methods were written in the Parent Person Class, Cristina would be able to call teach and Chris would be able to call learn because they inherit those methods.
